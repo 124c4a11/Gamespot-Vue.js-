c860beac83b3113b32a2d3d71d26fd2f95cb5bb1
124c4a11 <124c4a11@mail.ru>
2019-02-01 10:09:43 +0300

    Header: base markup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-layout column>
+    <app-header/>
+    <v-spacer>
+      <router-view/>
+    </v-spacer>
+  </v-layout>
 </template>
 
+<script>
+import AppHeader from '@/components/Header.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
 <style lang="scss">
+body { font: 16px 'Roboto', sans-serif; }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="page-header">
+    <v-container>
+      <v-layout row align-center>
+        <router-link to="/" class="logo">Gamespot</router-link>
+        <v-spacer/>
+        <router-link to="/signin" class="page-header__link">
+          <img :src="loginIcon" alt="login">
+        </router-link>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import loginIcon from '@/assets/img/login.png'
+
+export default {
+  data () {
+    return {
+      loginIcon
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/*================================================================
+  page-heder
+================================================================*/
+.page-header {  background-color: #343a40; }
+
+.page-header__link {
+  font-size: 1.25em;
+  color: #fff;
+  text-decoration: none;
+  opacity: .7;
+  transition-duration: .25s;
+
+  &:hover,
+  &:focus {
+    outline: none;
+    opacity: 1;
+  }
+
+  & + & { margin-left: 1em; }
+}
+
+/*================================================================
+  logo
+================================================================*/
+
+.logo {
+  font: 30px/1 'Bungee Inline', cursive;
+  color: #fff;
+  text-decoration: none;
+}
+</style>
