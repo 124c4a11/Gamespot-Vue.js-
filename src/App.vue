b6f09603c23f31bcd359bdb663b3aca4686f9aa0
124c4a11 <124c4a11@mail.ru>
2019-02-01 10:23:28 +0300

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <v-layout column>
+  <v-layout column class="page">
     <app-header/>
     <v-spacer>
       <router-view/>
     </v-spacer>
+    <app-footer/>
   </v-layout>
 </template>
 
 <script>
 import AppHeader from '@/components/Header.vue'
+import AppFooter from '@/components/Footer.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
 
 <style lang="scss">
 body { font: 16px 'Roboto', sans-serif; }
+
+.page { min-height: 100vh; }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="page-footer">
+    <v-container>
+      <div class="logo text-xs-center">Gamespot</div>
+    </v-container>
+  </footer>
+</template>
+
+<style>
+.page-footer { background-color: #343a40; }
+</style>
